6d839e1508436be82c17a75655415075bbe72e1a
WuJun <wujun19890209@gmail.com>
2017-08-28 14:45:37 +0800

    chore: merged.

---
+++ app.js
@@ -0,0 +1,23 @@
+'use strict';
+var express = require('express');
+var path = require('path');
+var AV = require('leanengine');
+
+var app = express();
+app.use(AV.express());
+app.enable('trust proxy');
+app.use(AV.Cloud.HttpsRedirect());
+app.use(express.static('dist'));
+
+// for system.js to work. Can be removed if bundling.
+// app.use(express.static(path.resolve(__dirname, '.')));
+// app.use(express.static(path.resolve(__dirname, '../node_modules')));
+
+var renderIndex = (req , res) => {
+    res.sendFile(path.resolve(__dirname, 'dist/index.html'));
+}
+
+app.get('/*', renderIndex);
+
+
+module.exports = app;
